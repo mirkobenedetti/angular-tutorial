d9950637a5a00b30b375478063a7fc33c41672e2
Mirko Benedetti <mirko.benedetti@gmx.com>
2018-08-25 17:09:10 +0200

    First Commit

---
+++ src/app/demo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-component',
+  templateUrl: './demo.component.html'
+})
+export class DemoComponent {
+  message = 'This is the demo component.';
+}
